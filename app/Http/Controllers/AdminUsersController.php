c7957e2287b1f6d81f7195c923b6cea99810314a
SaahilChawande <chawandesaahil600@gmail.com>
2018-03-20 21:48:52 +0530

    Password field and custom request

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -38,7 +39,7 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         return $request->all();
     }
