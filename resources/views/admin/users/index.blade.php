39af201ec6361ff87711f4785eec67ac9eda3bd9
SaahilChawande <chawandesaahil600@gmail.com>
2018-03-22 09:05:22 +0530

    Displaying photos using an accessor

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,6 +6,7 @@
         <thead>
             <tr>
                 <th>Id</th>
+                <th>Photo</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
@@ -19,6 +20,7 @@
                 @foreach($users as $user)
                     <tr>
                         <td>{{$user->id}}</td>
+                        <td><img height="50px" src="{{ $user->photo ? $user->photo->file : 'No User Photo' }}" alt="No User Photo"></td>
                         <td>{{$user->name}}</td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->role->name}}</td>
