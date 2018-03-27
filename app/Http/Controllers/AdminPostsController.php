3abf0dc6781dcde86ae8948a8a3deb84e0f55430
SaahilChawande <chawandesaahil600@gmail.com>
2018-03-27 18:23:07 +0530

    Displaying Post : Application - Posts

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class AdminPostsController extends Controller
@@ -13,7 +14,8 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
     <h1>Posts</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Post Id</th>
+                <th>User Id</th>
+                <th>Category Id</th>
+                <th>Photo Id</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+            </tr>
+        </thead>
+        <tbody>
+        @if($posts)
+            @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->user_id }}</td>
+                <td>{{ $post->category_id }}</td>
+                <td>{{ $post->photo_id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->body }}</td>
+                <td>{{ $post->created_at->diffForHumans() }}</td>
+                <td>{{ $post->updated_at->diffForHumans() }}</td>
+            </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
 @stop
