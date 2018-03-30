1a0ab6e6932163a0bc611af57d661f7d5db7b8ad
SaahilChawande <chawandesaahil600@gmail.com>
2018-03-31 00:22:48 +0530

    Displaying images in a post : Application - Posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,10 +5,10 @@
     <table class="table">
         <thead>
             <tr>
-                <th>Post Id</th>
+                <th>Id</th>
+                <th>Photo</th>
                 <th>Owner</th>
                 <th>Category</th>
-                <th>Photo</th>
                 <th>Title</th>
                 <th>Body</th>
                 <th>Created At</th>
@@ -20,9 +20,9 @@
             @foreach($posts as $post)
             <tr>
                 <td>{{ $post->id }}</td>
+                <td><img height="50px" src="{{ $post->photo ? $post->photo->file : 'https://picsum.photos/75/50/?random' }}" alt="No User Photo" class="img-rounded"></td>
                 <td>{{ $post->user->name }}</td>
                 <td>{{ $post->category_id }}</td>
-                <td><img height="50px" src="{{ $post->photo ? $post->photo->file : 'https://picsum.photos/75/50/?random' }}" alt="No User Photo" class="img-rounded"></td>
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->body }}</td>
                 <td>{{ $post->created_at->diffForHumans() }}</td>
