d9d08aaf3f758b943e3155a0518d6aff7dcb2fe6
SaahilChawande <chawandesaahil600@gmail.com>
2018-04-08 02:32:47 +0530

    Application - Comments : Relations and mass assignment

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['post_id', 'author', 'email', 'body', 'is_active'];
+    public function replies()   {
+        return $this->hasMany('App\CommentReply');
+    }
 }
