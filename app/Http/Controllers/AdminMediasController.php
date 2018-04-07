42227415aee33fbdf8cfa9ce6a8db936c7aa7de8
SaahilChawande <chawandesaahil600@gmail.com>
2018-04-08 01:33:43 +0530

    Uploading images

--- app/Http/Controllers/AdminMediasController.php
+++ app/Http/Controllers/AdminMediasController.php
@@ -15,4 +15,11 @@ class AdminMediasController extends Controller
     public function create()    {
         return view('admin.media.create');
     }
+
+    public function store(Request $request) {
+        $file = $request->file('file');
+        $name = time() . $file->getClientOriginalName();
+        $file->move('images', $name);
+        Photo::create(['file' => $name]);
+    }
 }
