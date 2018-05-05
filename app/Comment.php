a506fec6723268b8efd45840efe85e7684a92661
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-06 00:09:21 +0530

    Appication Comments : Displaying, Approving, Un-Approving and Deleting Comments

--- app/Comment.php
+++ app/Comment.php
@@ -10,4 +10,8 @@ class Comment extends Model
     public function replies()   {
         return $this->hasMany('App\CommentReply');
     }
+
+    public function post()  {
+        return $this->belongsTo('App\Post');
+    }
 }
--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -15,7 +15,8 @@ class PostCommentsController extends Controller
      */
     public function index()
     {
-        return view('admin.comments.index');
+        $comments = Comment::all();
+        return view('admin.comments.index', compact('comments'));
     }
 
     /**
@@ -80,7 +81,8 @@ class PostCommentsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        Comment::findOrFail($id)->update($request->all());
+        return redirect()->back();
     }
 
     /**
@@ -91,6 +93,7 @@ class PostCommentsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Comment::findOrFail($id)->delete();
+        return redirect()->back();
     }
 }
